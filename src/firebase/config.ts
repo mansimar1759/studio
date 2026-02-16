1f4850583f4bc6a883cfb318a61ee5e033f4a451
Mansimar Singh <mansimar1759@gmail.com>
2026-02-16 18:04:16 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,6 @@ export const firebaseConfig = {
   "appId": "1:36632513095:web:deb90741c396fdddbdff62",
   "apiKey": "AIzaSyBLvE4rBoeVUv8qrE7S6Uqjvd2chQAC73Q",
   "authDomain": "studio-9451076808-9ef6b.firebaseapp.com",
-  "measurementId": "",
+  "measurementId": "G-FHVL9F8FJ6",
   "messagingSenderId": "36632513095"
 };
